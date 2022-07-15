6132d6814927b9d291bb55c84d4c5209efff6063
julia-wink <109316266+julia-wink@users.noreply.github.com>
2022-07-14 22:05:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Duolingo</title>
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<u><a href="cad.php"> Cadastro</a></u>
+					<u><a href="log.php"> Login</a></u>
+				</ul>
+			</nav>
+			<h1>Duolingo</h1>
+				<button class="button-5" role="button">Comece agora!</button>
+
+		</header>
+		<article>
+			<h2>Jogo</h2><br>
+			<p>
+				aqui terão as informações do jogo
+			</p>
+		</article>
+
+
+	</body>
+</html>
